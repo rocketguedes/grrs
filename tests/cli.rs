014276b4a675ebbc4fb1a2502b03d943d97cd57e
Leandro Guedes <leanguedes@icloud.com>
2024-04-19 21:37:25 -0300

    testing cli applications by running them

---
+++ tests/cli.rs
@@ -0,0 +1,16 @@
+use std::process::Command;
+
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+
+#[test]
+fn file_dont_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("grrs")?;
+
+    cmd.arg("foobar").arg("test/file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("could not read file"));
+
+    Ok(())
+}
