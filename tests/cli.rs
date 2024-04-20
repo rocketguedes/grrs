8c697e33841286df36ff4ffae2ef5bee734ad103
Leandro Guedes <leanguedes@icloud.com>
2024-04-19 21:42:10 -0300

    generating test files

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,6 +1,7 @@
 use std::process::Command;
 
 use assert_cmd::prelude::*;
+use assert_fs::prelude::*;
 use predicates::prelude::*;
 
 #[test]
@@ -11,6 +12,20 @@ fn file_dont_exist() -> Result<(), Box<dyn std::error::Error>> {
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("could not read file"));
+
+    Ok(())
+}
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("A test\nActual content\nMore content\nAnother test")?;
+
+    let mut cmd = Command::cargo_bin("grrs")?;
+    cmd.arg("test").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("A test\nAnother test"));
 
     Ok(())
 }
